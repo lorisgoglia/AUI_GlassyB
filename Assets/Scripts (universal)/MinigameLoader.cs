620578f52c6f9026f761e8211b8b9a390dbecd0c
slowris <loris.goglia@gmail.com>
2022-12-17 19:38:30 +0100

    fixed loading of mars

--- Assets/Scripts (universal)/MinigameLoader.cs
+++ Assets/Scripts (universal)/MinigameLoader.cs
@@ -7,10 +7,11 @@ public class MinigameLoader : MonoBehaviour
     public GameObject sunnyScene;
     public GameObject uranusScene;
     public GameObject marsScene;
-
+    public FollowCamera followScript;
+    public GameObject rover;
+    public GameObject camera;
     private void Start()
     {
-        
     }
 
 
@@ -32,13 +33,29 @@ public class MinigameLoader : MonoBehaviour
         sunnyScene.SetActive(false);
         marsScene.SetActive(true);
         //aggiungere codice per spostare camera come figlio del rover
+        //followScript.enabled = true;
+        camera.transform.parent = rover.transform;
+        resetPosition();
+
+
+        //object1 is now the child of object2
     }
 
     public void MarsToSunny()
     {
+        camera.transform.parent = null;
         marsScene.SetActive(false);
         sunnyScene.SetActive(true);
         //aggiungere codice per spostare camera in (0,0,0)
+        //followScript.resetPosition();
+        //followScript.enabled = false;
+        resetPosition();
+
+    }
+
+    private void resetPosition()
+    {
+        camera.transform.position = new Vector3(0, 0, 0);
     }
 
 
