c647fa776c2dd94e91cabcec3c5be2fb4e87cb03
Francesco <dttfrancesco@gmail.com>
2022-12-23 18:26:14 +0100

    Introdotto sistema di pianeti vinti

---
+++ Assets/Uranus/Menu/PlanetsWonUranus.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlanetsWonUranus : MonoBehaviour
+{
+
+    public Text planetsWon;
+    UranusLife uranusLife;
+    [SerializeField] private GameObject uranus;
+    private const int  three = 3;
+    private const int two = 2;
+    private const int one = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       uranusLife = uranus.GetComponent<UranusLife>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlanetsWon();
+    }
+
+    public void PlanetsWon()
+    {
+        if (uranusLife.currentHealth == uranusLife.maxHealth) planetsWon.text = three.ToString();
+        else if (uranusLife.currentHealth < uranusLife.maxHealth && uranusLife.currentHealth >= (uranusLife.maxHealth / 2)) planetsWon.text = two.ToString();
+        else if (uranusLife.currentHealth < (uranusLife.maxHealth / 2) && uranusLife.currentHealth > 0) planetsWon.text = one.ToString();
+    }
+
+}
